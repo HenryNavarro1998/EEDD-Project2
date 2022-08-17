1ebaa277ed47eaf693cb46bb0e9db0a67c67ce4d
HenryNavarro1998 <henrymanuelnavarro@gmail.com>
2022-08-17 14:16:17 -0400

    before pull

--- main.cpp
+++ main.cpp
@@ -13,41 +13,9 @@
 
 using namespace std;
 
-struct Point
-{
-    int x;
-    int y;
-
-    Point(const int x, const int y):
-        x(x), y(y) {};
-};
-
-ostream& operator<<(ostream& stream, const Point& p)
-{
-    stream << "(" << p.x << "," << p.y << ")";
-    return stream;
-}
-
-inline bool operator==(const Point& p1, const Point& p2)
-{
-    return (p1.x == p2.x) && (p1.y == p2.y);
-}
-
-
 //Funcion Main
 int main()
 {
-
-    list<Point> l;
-
-    l.push_back(Point(1,2));
-    l.push_back(Point(1,5));
-    l.push_back(Point(3,2));
-    l.push_back(Point(-1,2));
-    l.push_back(Point(-1,-2));
-    l.push_back(Point(10,26));
-
-    cout << l.find(Point(-1,-2)) << endl;
 
     return 0;
 }
