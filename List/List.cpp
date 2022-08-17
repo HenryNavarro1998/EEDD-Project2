d86d36f7a6d906a50edc055593c9fed7bedd94db
Jorge <jalfredcaraballo@gmail.com>
2022-08-16 21:02:40 -0400

    final day 1

---
+++ List/List.cpp
@@ -0,0 +1,42 @@
+#include "List.hpp"
+#include <cstdlib>
+
+using namespace std;
+
+bool List::push(const Client& client){
+
+    Node* node = new Node(client);
+
+    if(empty())
+        first = node;
+    else
+        last->next = node;
+
+    last = node;
+    length++;
+    return true;
+}
+
+bool List::pop(){
+
+    if(empty())
+        return false;
+
+    Node* aux = first;
+    first = first->next;
+
+    delete aux;
+    length--;
+    return true;
+}
+
+ostream& operator<<(ostream& stream, const Client& c){
+    stream << c.name;
+    return stream;
+}
+
+ostream& operator<<(ostream& stream, const List& l){
+    for(Node* i = l.first; i; i = i->next)
+        stream << i->info << endl;
+    return stream;
+} 
---
+++ List/List.hpp
@@ -0,0 +1,50 @@
+#ifndef List_hpp
+#define List_hpp
+
+//listas con clases funciones 
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+struct Client{
+
+    string name;
+
+    Client(const string& n=""): name(n) {};
+};
+
+struct Node{
+
+    Client info;
+    Node* next;
+    
+    Node(const Client& c=Client(), Node* n=NULL): info(c), next(n){};
+};
+
+//Clase Lista
+class List{
+
+    //Seccion Privada
+    private:
+        Node *first, *last; // Cabeza y Cola de la Lista
+        int length;
+        friend ostream& operator<<(ostream&, const List&); // Sobrecarga del operador '<<''
+
+    public:
+        List(): first(NULL), last(NULL), length(0){}; // Constructor
+        ~List() { while(pop()); }; // Destructor
+
+        bool empty() const{ return first == NULL; }; //Lista vacia
+        
+        bool push(const Client&); //Inserta en Orden las palabras
+        bool pop(); //Elimina una palabra
+        
+        const Client& front() const { return first->info; };
+};
+
+//Sobrecarga del operador '<<' para las palabras y las listas
+ostream& operator<<(ostream&,const Client&);
+ostream& operator<<(ostream&,const List&);
+
+#endif
